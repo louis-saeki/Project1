fe84cde5e6e444a0affb88d8df774cca19730094
Nick Wilson <nickedges@gmail.com>
2023-06-06 17:16:46 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,10 @@ function sayHello(){
     const input = document.getElementById("userInput").value;
     alert(`Hello ${input}! How are you today?`)
 }
+
+
+function sayHelloToFriend(){
+    const input = document.getElementById("myFriend").value;
+    alert(`Hello ${input}! How are you today?`)
+}
+
