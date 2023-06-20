8564c120ebfc19aaf5fb4f639df28369ad36075e
louis-saeki <130442537+louis-saeki@users.noreply.github.com>
2023-06-20 16:35:32 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,16 @@ function updateTable() {
     document.getElementById('table1').rows[3].cells[2].innerText = total3.toString();
 }
 
+function updateTable2() {
+    const input = parseInt(document.getElementById("table2-input").value);
+    const val1 = parseInt(document.getElementById('r1c2-value').innerHTML);
+    const val2 = parseInt(document.getElementById('r2c2-value').innerHTML);
+    const val3 = parseInt(document.getElementById('r3c2-value').innerHTML);
+    const total1 = val1 * input;
+    const total2 = val2 * input;
+    const total3 = val3 * input;
+    document.getElementById('table1').rows[1].cells[2].innerText = total1.toString();
+    document.getElementById('table1').rows[2].cells[2].innerText = total2.toString();
+    document.getElementById('table1').rows[3].cells[2].innerText = total3.toString();
+}
+
