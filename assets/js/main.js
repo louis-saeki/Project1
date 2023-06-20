3e4e5d4a2d33fb64ae2f74159069c11faecf8f9b
Nick Wilson <nickedges@gmail.com>
2023-06-20 16:42:35 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,14 +24,15 @@ function updateTable() {
 
 function updateTable2() {
     const input = parseInt(document.getElementById("table2-input").value);
-    const val1 = parseInt(document.getElementById('r1c2-value').innerHTML);
-    const val2 = parseInt(document.getElementById('r2c2-value').innerHTML);
-    const val3 = parseInt(document.getElementById('r3c2-value').innerHTML);
+    const el = document.getElementById('table2');
+    const val1 = parseInt(el.rows[1].cells[1].innerHTML);
+    const val2 = parseInt(el.rows[2].cells[1].innerHTML);
+    const val3 = parseInt(el.rows[3].cells[1].innerHTML);
     const total1 = val1 * input;
     const total2 = val2 * input;
     const total3 = val3 * input;
-    document.getElementById('table1').rows[1].cells[2].innerText = total1.toString();
-    document.getElementById('table1').rows[2].cells[2].innerText = total2.toString();
-    document.getElementById('table1').rows[3].cells[2].innerText = total3.toString();
+    el.rows[1].cells[2].innerText = total1.toString();
+    el.rows[2].cells[2].innerText = total2.toString();
+    el.rows[3].cells[2].innerText = total3.toString();
 }
 
