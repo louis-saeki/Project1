5f29f850a6905ebbcab91a0b8fc0f4859a85368a
louis-saeki <130442537+louis-saeki@users.noreply.github.com>
2023-08-29 16:15:31 +0900

    Create auth.js

---
+++ assets/js/auth.js
@@ -0,0 +1,16 @@
+const loginButton = document.getElementById("login-form-submit");
+4loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const loginForm = document.getElementById("login-form");
+  const loginErrorMsgHolder = document.getElementById("login-error-msg-holder");
+  const username = loginForm.username.value;
+  const password = loginForm.password.vaue;
+
+  if (username === "user" && password === "password") {
+    alert("You have successfully logged in.")
+    location.reload();
+  } else {
+    alert("Invalid username or password!");
+    loginErrorMsgHolder.style.display = "block";
+  }
+}
